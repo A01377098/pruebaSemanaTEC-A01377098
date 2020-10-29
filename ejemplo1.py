db8a5d66f9b28e6edb08638f392075145602b4ed
A01377098 <A01377098@itesm.mx>
2020-10-29 13:09:15 -0600

    Cambios

--- ejemplo1.py
+++ ejemplo1.py
@@ -10,10 +10,10 @@ imagen = cv2.resize(imagen, (256, 256))
 cv2.imwrite("resizeimagen.jpg", imagen)
 #cv2.imshow("image", imagen)
 
-imagen = cv2.imread("imagen,jp")
+imagen = cv2.imread("imagen.jpg")
 imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
 print (imagen.shape)
-print (imagen[0][0])
+print (imagen[0][0]) 
 cv2.imwrite("grayimagen.jpg", imagen)
 
 
